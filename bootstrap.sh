7a1d30d06386460b1b48c6bc66aa65a1baac4160
Stanislav Otovchits <otov4its@gmail.com>
2015-06-28 23:25:47 +0600

    Setup Vundle.git

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,3 +23,7 @@ for file in $files; do
 	echo "Creating symlink to $file in home directory."
 	ln -s $dir/$file ~/$file
 done
+
+# Setup Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
